afb5226843ab73e6c498105e80ddecbb1d3cb5a3
Jared Rice Sr <jaredricelegal@gmail.com>
2018-08-09 18:35:28 -0500

    Pushed to 0.3.3

--- index.js
+++ index.js
@@ -1,51 +1,50 @@
 var exec = require('child_process').exec
 var crypto = require('crypto')
 var os = require('os')
-var dPackLogger = require('@dpack/logger')
-var result = require('@dpack/logger/result')
-var dPackJobs = require('@dpack/jobs')
+var dPackLog = require('neat-log')
+var result = require('neat-log/output')
+var dPackTasks = require('neat-tasks')
 var chalk = require('chalk')
 var Menu = require('menu-string')
 var debug = require('debug')('drsatoshi')
-var defaultJobs = require('./lib/jobs-default')
-var peerJobs = require('./lib/jobs-peer')
+var defaultTasks = require('./lib/tasks-default')
+var peerTasks = require('./lib/tasks-peer')
 var peerTest = require('./lib/peer-test')
-
 var NODE_VER = process.version
 var DOCTOR_VER = require('./package.json').version
-var DPACK_PROCESS = process.title === 'dpack'
+var DWEB_PROCESS = process.title === 'dweb'
 
 module.exports = function (opts) {
   if (!opts) opts = {}
   opts.peerId = opts.peerId || null
-  opts.port = typeof opts.port === 'number' ? opts.port : 6620
+  opts.port = typeof opts.port === 'number' ? opts.port : 6200
 
   var views = [headerOutput, versionsOutput, menuView]
-  var log = dPackLogger(views)
-  log.use(getVersions)
+  var neat = dPackLog(views)
+  neat.use(getVersions)
 
   if (opts.peerId) return runP2P() // run p2p tests right away
 
   var menu = Menu([
-    'Basic dPack Tests (Checks your dPack installation and network setup)',
-    'dWeb P2P Network Test (Debug connections between two computers)'
+    'Basic dWeb Tests (Checks your dPack installation and network setup)',
+    'Peer-to-Peer dWeb Test (Debug connections between two computers)'
   ])
-  log.use(function (state) {
+  neat.use(function (state) {
     state.opts = opts
     state.port = opts.port
   })
-  log.use(function (state, bus) {
+  neat.use(function (state, bus) {
     bus.emit('render')
 
-    log.input.on('down', function () {
+    neat.input.on('down', function () {
       menu.down()
       bus.render()
     })
-    log.input.on('up', function () {
+    neat.input.on('up', function () {
       menu.up()
       bus.render()
     })
-    log.input.once('enter', function () {
+    neat.input.once('enter', function () {
       state.selected = menu.selected()
       bus.render()
       startTests(state.selected)
@@ -58,10 +57,10 @@ module.exports = function (opts) {
   }
 
   function runBasic () {
-    var runJobs = dPackJobs(defaultJobs(opts))
-    views.push(runJobs.view)
-    log.use(runJobs.use)
-    log.use(function (state, bus) {
+    var runTasks = dPackTasks(defaultTasks(opts))
+    views.push(runTasks.view)
+    neat.use(runTasks.use)
+    neat.use(function (state, bus) {
       bus.once('done', function () {
         var testCountMsg = result(`
           ${chalk.bold(state.pass)} of ${chalk.bold(state.totalCount)} tests passed
@@ -78,8 +77,8 @@ module.exports = function (opts) {
         console.log(result(`
           ${chalk.bold.redBright('FAIL')}
           ${testCountMsg}
-          Your network may be preventing you from using Dat.
-          For further troubleshooting, visit https://docs.dpack.io/issues
+          Your network may be preventing you from using the dWeb.
+          For further troubleshooting, visit http://docs.dpacks.io/troubleshooting
         `))
         process.exit(1)
       })
@@ -90,19 +89,19 @@ module.exports = function (opts) {
     if (opts.peerId) {
       opts.existingTest = true
       opts.id = opts.peerId
-      return startJobs()
+      return startTasks()
     }
 
     opts.existingTest = false
     opts.id = crypto.randomBytes(32).toString('hex')
-    startJobs()
+    startTasks()
 
-    function startJobs () {
-      var runJobs = dPackJobs(peerJobs(opts))
-      views.push(runJobs.view)
-      log.use(runJobs.use)
-      log.use(function (state, bus) {
-        // initial jobs done
+    function startTasks () {
+      var runTasks = dPackTasks(peerTasks(opts))
+      views.push(runTasks.view)
+      neat.use(runTasks.use)
+      neat.use(function (state, bus) {
+        // initial tasks done
         bus.once('done', function () {
           // TODO: Fix, overwriting previous line
           views.push(function () { return '\n' })
@@ -117,7 +116,7 @@ module.exports = function (opts) {
   }
 
   function headerOutput (state) {
-    return `Welcome to ${chalk.greenBright('Dr Satoshi')} for dPack\n`
+    return `Welcome to ${chalk.greenBright('Dr')} Satoshi!\n`
   }
 
   function menuView (state) {
@@ -143,31 +142,31 @@ module.exports = function (opts) {
       Software Info:
         ${os.platform()} ${os.arch()}
         Node ${version.node}
-        Dr Satoshi v${version.doctor}
-        ${dpackVer()}
+        Dr. Satoshi v${version.satoshi}
+        ${dwebVer()}
     `) + '\n'
 
-    function dpackVer () {
-      if (!DPACK_PROCESS || !version.dpack) return ''
-      return chalk.green(`dpack v${version.dpack}`)
+    function dwebVer () {
+      if (!DWEB_PROCESS || !version.dweb) return ''
+      return chalk.green(`dweb v${version.dweb}`)
     }
   }
 
   function getVersions (state, bus) {
     state.versions = {
-      dpack: null,
+      dweb: null,
       satoshi: DOCTOR_VER,
       node: NODE_VER
     }
-    exec('dpack -v', function (err, stdin, stderr) {
+    exec('dweb -v', function (err, stdin, stderr) {
       if (err && err.code === 127) {
-        // Dat not installed/executable
-        state.dpackInstalled = false
+        // dPack not installed/executable
+        state.dwebInstalled = false
         return bus.emit('render')
       }
       // if (err) return bus.emit('render')
-      // TODO: right now dpack -v exits with error code, need to fix
-      state.versions.dpack = stderr.toString().split('\n')[0].trim()
+      // TODO: right now dweb -v exits with error code, need to fix
+      state.versions.dweb = stderr.toString().split('\n')[0].trim()
       bus.emit('render')
     })
   }
